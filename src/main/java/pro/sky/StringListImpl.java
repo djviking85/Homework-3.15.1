43a597e15a1086c842310540ac53f73b76e6512d
Viking <djviking@yandex.ru>
2023-04-12 17:07:09 +0300

    добавляем ошибки

--- src/main/java/pro/sky/StringListImpl.java
+++ src/main/java/pro/sky/StringListImpl.java
@@ -1,5 +1,72 @@
 package pro.sky;
 
 public class StringListImpl implements StringList {
+    String[] strtingArrayList = new String[];
+
+    @Override
+    public String add(String item) {
+        if(item != null)
+    }
+
+
+    @Override
+    public String set(int index, String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(String item) {
+        return null;
+    }
+
+    @Override
+    public String remove(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(String item) {
+        return false;
+    }
+
+    @Override
+    public int indexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(String item) {
+        return 0;
+    }
+
+    @Override
+    public String get(int index) {
+        return null;
+    }
+
+    @Override
+    public boolean equals(StringList otherList) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public String[] toArray() {
+        return new String[0];
+    }
 
 }
